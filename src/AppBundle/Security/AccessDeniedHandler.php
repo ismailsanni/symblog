954cfb741fd39ea7b175b53a80670207b4d5e4a1
Ismail Sanni <sanni920@gmail.com>
2019-02-17 22:55:35 +0100

    admin access denied handler setup

---
+++ src/AppBundle/Security/AccessDeniedHandler.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace AppBundle\Security;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+use Symfony\Component\Security\Http\Authorization\AccessDeniedHandlerInterface;
+
+class AccessDeniedHandler implements AccessDeniedHandlerInterface
+{
+    private $templating;
+
+    public function __construct(\Twig_Environment $templating)
+    {
+        $this->templating = $templating;
+    }
+
+    public function handle(Request $request, AccessDeniedException $accessDeniedException)
+    {
+        return new Response($this->templating->render('security/access-denied.html.twig'));
+    }
+
+}
+
+
+
